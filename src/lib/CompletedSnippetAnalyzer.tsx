e7b658db5b16ea0b50a9b16fb95da6fb028fc208
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-11-07 17:16:32 +0530

    formatting

--- src/lib/CompletedSnippetAnalyzer.tsx
+++ src/lib/CompletedSnippetAnalyzer.tsx
@@ -1,9 +1,9 @@
-import { IKeystrokeLog } from './KeystrokeRecorder';
+import { IKeystrokeLog } from "./KeystrokeRecorder";
 import * as _ from "lodash";
 
 class CompletedSnippetAnalyzer {
-    snippetText: string
-    keystrokeLogs: IKeystrokeLog[]
+    snippetText: string;
+    keystrokeLogs: IKeystrokeLog[];
 
     constructor(snippetText: string, keystrokeLogs: IKeystrokeLog[]) {
         this.snippetText = snippetText;
@@ -12,11 +12,12 @@ class CompletedSnippetAnalyzer {
 
     public averageSpeed(): number {
         let firstLog = this.keystrokeLogs[0];
-        let lastLog = this.keystrokeLogs[this.keystrokeLogs.length-1]
+        let lastLog = this.keystrokeLogs[this.keystrokeLogs.length - 1];
         let firstLogTimestamp = firstLog.timestamp;
         let lastLogTimestamp = lastLog.timestamp;
 
-        let durationMilliseconds = lastLogTimestamp.getTime() - firstLogTimestamp.getTime();
+        let durationMilliseconds =
+            lastLogTimestamp.getTime() - firstLogTimestamp.getTime();
         let durationMinutes = durationMilliseconds / (1000 * 60);
 
         let charCount = this.snippetText.length;
@@ -25,13 +26,17 @@ class CompletedSnippetAnalyzer {
         return Math.round(cpm / 5);
     }
 
+    public speedsAtIndices(): number[] {
+        return [0];
+    }
+
     public mistakeIndices(): number[] {
         var currentIndex = 0;
         var mistakeIndices: number[] = [];
         var lastIndexWasMistake = false;
         _.forEach(this.keystrokeLogs, function(log) {
             if (log.key.type == "backspace" && !lastIndexWasMistake) {
-                mistakeIndices.push(currentIndex - 1)
+                mistakeIndices.push(currentIndex - 1);
             }
 
             if (log.key.type == "character") {
